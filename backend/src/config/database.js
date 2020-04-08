860d184ee866444f2a36d4d01fd0bf6bdedc1f9f
bsignx <bsignx@gmail.com>
2020-04-08 17:54:54 -0300

    first

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '16052001',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
